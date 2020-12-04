69022d9cba6adaf5f119e4db51e4d5facd4394fc
m9810223 <39259397+m9810223@users.noreply.github.com>
2020-12-04 20:12:57 +0800

    Update Day 2: Password Philosophy.py

--- Day 2: Password Philosophy.py
+++ Day 2: Password Philosophy.py
@@ -1,8 +1,7 @@
-with open('input.txt', 'r') as f:
+with open('inputD2.txt', 'r') as f:
     data = f.read()
-data = data.replace(':', '')
-data = data.split('\n')
-data = [x.split(' ')[0].split('-')+x.split(' ')[1:] for x in data]
+data = data.replace(':', '').replace('-',' ').split('\n')
+data = [x.split(' ') for x in data]
 
 
 def chk1(a, b, c, d):
@@ -29,9 +28,13 @@ for a, b, c, d in data:
     if chk1(a, b, c, d):
         re += 1
 print('Sol1:', re)
+# Sol1: 660
+
 
 re = 0
 for a, b, c, d in data:
     if chk2(a, b, c, d):
         re += 1
 print('Sol2:', re)
+# Sol2: 530
+
